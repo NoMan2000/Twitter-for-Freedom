3c2579bd7ebc29af0f495b981b5f9536d5ea88d6
Michael Ryan Soileau <msoile7@lsu.edu>
2012-02-16 10:07:49 -0600

    Finishing up the routes and config documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp2::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', to: 'users#new'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
