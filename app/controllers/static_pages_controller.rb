f041b432c235a8fdbf88f6ba202bba64c51ddb27
Michael Ryan Soileau <msoile7@lsu.edu>
2012-02-16 23:51:10 -0600

    Basically done, time to style and add some more goodies to it.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-  	  if signed_in?
-      @micropost  = current_user.microposts.build
+  	 @title = "Home"
+    if signed_in?
+      @micropost = Micropost.new
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,15 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   before_save :create_remember_token
+  
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+ has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   validates :name,  presence: true, length: { maximum: 50 }
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -23,11 +31,25 @@ class User < ActiveRecord::Base
   			uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6}
 
+
+
+
+
 def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
-  
+
+def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end  
+
+   def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
 
 private
 
