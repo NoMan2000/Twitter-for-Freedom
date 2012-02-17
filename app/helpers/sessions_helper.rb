fa07aeacbdb3dbb2dad9a22bc2763c2bb6a554b9
Michael Ryan Soileau <msoile7@lsu.edu>
2012-02-16 21:26:18 -0600

    Fixed the Session helper to store microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,7 +33,12 @@ def current_user
     session[:return_to] = request.fullpath
   end  
 
-
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
 
   	private
 	def user_from_remember_token
